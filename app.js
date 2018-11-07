28d7d04093cf58c70bdee01ab47b73943cd7dc6b
Egon Fiedler <efiedler93@gmail.com>
2018-11-07 11:00:10 -0800

    added post route

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const redis = require('redis');
 
+// Create Redis Client
+let client = redis.createClient();
+
+client.on('connect', function(){
+  console.log('Connected to Redis');
+});
+
 const port = 3000;
 
 const app = express();
@@ -17,8 +24,29 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(methodOverride('_method'));
 
+// Search page
 app.get('/', function(req, res, next){
   res.render('searchusers');
+  console.log("get '/', render 'searchusers'");
+  console.log('----------------------------------------');
+});
+
+// Search processing
+app.post('/user/search', function(req, res, next){
+  let id = req.body.id;
+
+  client.hgetall(id, function(err, obj){
+    if(!obj){
+      res.render('searchusers', {
+        error: 'User does not exist'
+      });
+    } else {
+      obj.id = id;
+      res.render('details', {
+        user: obj
+      })
+    }
+  });
 });
 
 app.listen(port, function(){
